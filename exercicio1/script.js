340634a290c22b6c2a2624fe912da273a98c9c55
Atreus66 <brunoragnar06@gmail.com>
2022-07-27 18:57:09 -0300

    Novas atribuicões

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -13,8 +13,16 @@ Você deve usar apenas um `console.log()` para isso*/
 
 
 const user = prompt("Qual o seu nome?")
-const nomeComida1 = prompt("Qual sua comida favorita?")
-const nomeComida2 = prompt("Qual sua comida favorita?")
-const nomeComida3 = prompt("Qual sua comida favorita?")
+const nomeComida1 = prompt("Qual sua primeira comida favorita?")
+const nomeComida2 = prompt("Qual sua segunda comida favorita?")
+const nomeComida3 = prompt("Qual sua terceira comida favorita?")
 
 console.log(`Essas são as comidas favoritas do ${user} \n${nomeComida1} \n${nomeComida2} \n${nomeComida3}`)
+
+/*let suaComida = 'Digite sua primeira comida favorita'
+const nome = prompt("Digite seu nome")
+const comida1 = prompt(suaComida)
+const comida2 = prompt(suaComida.replace("primeira", "segunda"))
+const comida3 = prompt(suaComida.replace("primeira", "terceira"))
+const mensagem = `Estas são as comidas favoritas de ${nome} \n- ${comida1} \n- ${comida2} \n- ${comida3}`
+console.log(mensagem)*/
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -17,15 +17,30 @@ c) Verifique se a nova string inclui **verde**, e se inclui **laranja**.
 
 //Crie a const para a frase aqui
 
-const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: BOAS VINDAS, mas não deixe o gato sair"
+const frase = `\"Jorge tem uma casa verde e com portão azul, com os dizeres: BOAS VINDAS, mas não deixe o gato sair\"`
+console.log(frase)
 
+// Aqui está ocorrendo a substituição das cores
 const novaFrase = frase.replace ("verde","rosa")
 console.log(novaFrase)
 const fraseFinal = novaFrase.replace("azul","laranja")
 console.log(fraseFinal)
 
-console.log(fraseFinal.includes("verde","laranja"))
+// Aqui está verificando as cores "verde" E "Laranja" separadamente 
+
+console.log(fraseFinal.includes("verde"))
+console.log(fraseFinal.includes("laranja"))
 
 const parte1 = "Jorge tem uma casa rosa e com portão laranja, com os dizeres:"
 const parte2 = "BOAS VINDAS, mas não deixe o gato sair"
 console.log(parte1, parte2.toUpperCase())
+
+//const novaString2 = frase.replace("mas não deixe o gato sair", (`${"mas não deixe o gato sair" .toUppercase()}`))
+//console.log(novaString2)
+
+console.log(`${"oi".toUpperCase()}`)
+//dentro da tua ${}, bota uma aspa dupla ${"texto".toUpperCase()}
+
+const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
+
+console.log(frase.replace("mas não deixe o gato sair", `${"mas não deixe o gato sair".toUpperCase()}`))
